0604203e2c87dbcfa24501feff8bfa16f9a778db
yamayuu0406 <ta.yu.2001.0406@gmail.com>
2018-12-14 17:35:38 +0900

    motion2の修正をしました。

--- examples/motion2.rb
+++ examples/motion2.rb
@@ -4,7 +4,7 @@ Stage.new("Stage",
           costumes: [
             {
               asset_id: "cd21514d0531fdffb22204e0ec5ed84a",
-              name: "背景1",
+              name: "backdrop1",
               bitmap_resolution: 1,
               data_format: "svg",
               rotation_center_x: 240,
@@ -18,21 +18,19 @@ Stage.new("Stage",
           ]) do
 end
 
-Sprite.new("スプライト1",
-           x: -124.12359052154574,
-           y: 130.17724550778007,
-           direction: -54.58450204512181,
+Sprite.new("Sprite1",
            costumes: [
              {
                asset_id: "01ae57fd339529445cb890978ef8a054",
-               name: "コスチューム1",
+               name: "costume1",
                bitmap_resolution: 1,
                data_format: "svg",
-               rotation_center_x: 47,
-               rotation_center_y: 55
+               rotation_center_x: 46,
+               rotation_center_y: 53
              }
            ]) do
-  self.when(:flag_clicked) do
-    point_towards("_mouse_")
-  end
+  point_towards("_mouse_")
+  go_to("_random_")
+  self.x += 10
+  self.y += 10
 end
